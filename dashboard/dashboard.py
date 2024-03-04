53d0a1b810cb94dd1664b5bd55a3d8c47d134a42
MichaelBolang <159759964+MichaelBolang@users.noreply.github.com>
2024-03-04 16:34:20 +0700

    Update dashboard.py
    Remove some comment and change variable name

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,26 +2,24 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# LOAD DATA
 @st.cache_resource
-def load_data():
-    data = pd.read_csv("data/day.csv")
-    return data
-data = load_data()
+def aksesdata():
+    set_day = pd.read_csv("data/day.csv")
+    return set_day
+ds = aksesdata()
 
-# TITLE DASHBOARD
-# Set page title
+#Title
 st.title("Dashboard Streamlit Bike Sharing")
 
-# VISUALIZATION
+# Visualisasi Data
 # ### Pertanyaan 1: Bagaimana statistik rental sepeda di tahun 2012?
-year_target = data.query('yr == 1')
+year_target = ds.query('yr == 1')
 statistik_2012 = year_target.groupby("mnth")["cnt"].sum().reset_index()
 fig = px.line(statistik_2012, x="mnth", y="cnt", title="Statistik Rental Sepeda pada Tahun 2012").update_layout(xaxis_title="Bulan", yaxis_title="Jumlah Rental Sepeda")
 st.plotly_chart(fig, use_container_width=True,height=400, width=600)
 
 # ### Pertanyaan 2: Apa musim yang paling banyak terjadi rental sepeda di tahun 2011?
-year_target = data.query('yr == 1')
+year_target = ds.query('yr == 1')
 season_2011 = year_target.groupby("season")["cnt"].sum().reset_index()
 fig = px.bar(season_2011, x="season",y="cnt", title="Jumlah Sewa Sepeda Berdasarkan Musim pada Tahun 2011").update_layout(xaxis_title="Musim", yaxis_title="Jumlah Rental Sepeda")
 st.plotly_chart(fig, use_container_width=True,height=400, width=600)
